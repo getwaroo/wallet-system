5bf1020916ed1d4539c3c9001a77e254450bd6fe
getwaroo <94100109+getwaroo@users.noreply.github.com>
2024-06-02 10:58:58 +0400

    Add files via upload

---
+++ src/main/java/com/example/wallet_system/WalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wallet_system/WalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
