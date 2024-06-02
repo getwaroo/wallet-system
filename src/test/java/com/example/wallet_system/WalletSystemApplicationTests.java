32cd7093f7657b2dea470e68ec9418ec315c1622
getwaroo <avishetwaroo@gmail.com>
2024-06-03 02:09:20 +0400

    correct unit test.

--- src/test/java/com/example/wallet_system/WalletSystemApplicationTests.java
+++ src/test/java/com/example/wallet_system/WalletSystemApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.wallet_system;
 
 import com.example.wallet_system.client.WalletGrpcClient;
 import com.example.wallet_system.grpc.WalletGrpcServiceImpl;
+import com.example.wallet_system.server.entity.User;
+import com.example.wallet_system.server.entity.Wallet;
+import com.example.wallet_system.server.repository.UserRepository;
 import com.example.wallet_system.server.repository.WalletRepository;
 import com.example.wallet_system.server.service.WalletService;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,50 +14,100 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringBootWalletGrpcApplicationTests {
 
-	@Autowired
-	private WalletGrpcServiceImpl walletGrpcServiceImpl;
+    @Autowired
+    private WalletGrpcServiceImpl walletGrpcServiceImpl;
 
-	@Autowired
-	private WalletService walletService;
+    @Autowired
+    private WalletService walletService;
 
-	@Autowired
-	private WalletRepository walletRepository;
+    @Autowired
+    private WalletRepository walletRepository;
 
-	private static WalletGrpcClient walletGrpcClient;
+    @Autowired
+    private UserRepository userRepository;
 
-	@BeforeAll
-	static void setup() {
-		walletGrpcClient = new WalletGrpcClient();
-	}
+    private static WalletGrpcClient walletGrpcClient;
 
-	@Test
-	void contextLoads() {
-		assertThat(walletGrpcServiceImpl).isNotNull();
-		assertThat(walletService).isNotNull();
-		assertThat(walletRepository).isNotNull();
-	}
+    @BeforeAll
+    static void setup() {
+        walletGrpcClient = new WalletGrpcClient();
+    }
 
-	@Test
-	void creditWalletGrpcServiceTest() {
-		String userId = "testuser";
-		double amount = 100.0;
-		String responseMessage = walletGrpcClient.creditWallet(userId, amount);
-		assertThat(responseMessage).isEqualTo("Amount credited successfully");
+    @Test
+    void contextLoads() {
+        assertThat(walletGrpcServiceImpl).isNotNull();
+        assertThat(walletService).isNotNull();
+        assertThat(walletRepository).isNotNull();
+    }
 
-		double balance = walletGrpcClient.getBalance(userId);
-		assertThat(balance).isEqualTo(100.0);
-	}
+    @Test
+    void creditWalletGrpcServiceTest() {
 
-	@Test
-	void getBalanceGrpcServiceTest() {
-		String userId = "testuser";
-		double balance = walletGrpcClient.getBalance(userId);
-		assertThat(balance).isEqualTo(100.0);
-	}
+        // Arrange
+        User user = new User();
+        user.setIdNumber("12345");
+        user.setName("TestUser");
+        user.setAddress("Flacq");
+        userRepository.save(user);
+
+        double amount = 100.0;
+
+        String responseMessage = walletGrpcClient.creditWallet(user.getIdNumber(), amount);
+        assertThat(responseMessage).isEqualTo("Amount credited successfully");
+
+        // Assert
+        Optional<Wallet> walletOpt = Optional.ofNullable(walletRepository.findByIdNumber(user.getIdNumber()));
+        assertThat(walletOpt).isNotNull();
+        assertThat(walletOpt.get().getAmount()).isEqualTo(amount);
+        assertThat(walletOpt.get().getDate()).isEqualTo(LocalDate.now());
+        assertThat(walletOpt.get().getUser()).isEqualTo(user);
+    }
+
+    @Test
+    void creditWalletGrpcServiceTestInvalidUser() {
+
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> walletService.creditWallet("1245", 1500),
+                "Expected creditWallet to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("User not found"));
+
+    }
+
+    @Test
+    void getBalanceGrpcServiceTest() {
+        User user = new User();
+        user.setIdNumber("4722");
+        user.setName("TestUserBal");
+        user.setAddress("Curepipe");
+        userRepository.save(user);
+
+        double amount = 1500;
+
+        walletGrpcClient.creditWallet(user.getIdNumber(), amount);
+        double amountQuery = walletGrpcClient.getBalance(user.getIdNumber());
+        assertThat(amount).isEqualTo(amountQuery);
+    }
+
+    @Test
+    void getBalanceGrpcServiceTestInvalidUser() {
+        RuntimeException thrown = assertThrows(
+                RuntimeException.class,
+                () -> walletService.getBalance("invalid_id"),
+                "Expected getBalance to throw, but it didn't"
+        );
+        assertTrue(thrown.getMessage().contains("Invalid id number"));
+    }
 }
